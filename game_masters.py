c65cef30b4af0c420b894f8377cc4da124b72892
Lazar Gueorguiev <lazargueorguiev2021@u.northwestern.edu>
2019-02-07 15:40:31 -0600

    v_1.0

--- game_masters.py
+++ game_masters.py
@@ -1,27 +1,25 @@
 from logical_classes import *
 from kb_and_inference_engine import *
 from read import *
-from abc import ABC, abstractmethod
 import os
 
-class GameMaster(ABC):
 
-    TXTS_DIRECTORY_PATH='flatfiles'
+class GameMaster(object):
+    TXTS_DIRECTORY_PATH = 'flatfiles'
 
     def __init__(self):
         self.kb = KnowledgeBase([], [])
         self.moveableQuery = self.produceMovableQuery()
 
-    @abstractmethod
     def produceMovableQuery(self):
-        raise NotImplementedError('Subclasses must override produceMovableQuery() '\
-            'to provide the query for facts starting with MOVABLE predicate')
+        raise NotImplementedError('Subclasses must override produceMovableQuery() ' \
+                                  'to provide the query for facts starting with MOVABLE predicate')
 
-    def getMovables(self):        
+    def getMovables(self):
         listOfBindings = self.kb.kb_ask(self.moveableQuery)
         if listOfBindings:
-            return [instantiate(self.moveableQuery.statement,bindings)\
-                     for bindings in listOfBindings]
+            return [instantiate(self.moveableQuery.statement, bindings) \
+                    for bindings in listOfBindings]
         else:
             return listOfBindings
 
@@ -30,13 +28,15 @@ class GameMaster(ABC):
         for fr in read_tokenize(final_path):
             self.kb.kb_assert(fr)
 
+
 class TowerOfHanoiGame(GameMaster):
 
     def __init__(self):
         super().__init__()
-        
+
     def produceMovableQuery(self):
         return parse_input('fact: (movable ?disk ?init ?target)')
+
 
 class Puzzle8Game(GameMaster):
 
@@ -45,5 +45,4 @@ class Puzzle8Game(GameMaster):
 
     def produceMovableQuery(self):
         return parse_input('fact: (movable ?piece ?initX ?initY ?targetX ?targetY)')
-
 
